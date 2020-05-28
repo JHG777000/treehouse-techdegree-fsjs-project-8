9bcde7fad46c1611f2e8132eba2599e7a4ac751c
JHG777000 <jhg777000@gmail.com>
2020-05-27 22:36:18 -0400

    fix 404

--- app.js
+++ app.js
@@ -11,24 +11,15 @@ const books = require('./routes/books');
 app.set('view engine', 'pug');
 
 app.use(express.urlencoded({ extended: false }));
-app.use('/static',express.static('public'));
+app.use('/static', express.static('public'));
 
 app.use('/', books);
 
-app.use((req,res,next) => {
-    const err =  new Error('The page you requested, can not be found.');
-    err.status = 404;
-    console.log(err.status + ': The page you requested, can not be found.');
-    //res.render('page-not-found');
-    next(err);
-});
-
-app.use((err,req,res,next) => {
-    res.locals.error = err;
-    res.status(err.status);
-    res.render('error');
+app.use(function (req, res, next) {
+  res.status(404);
+  res.render('page-not-found');
 });
 
 app.listen(3000, () => {
-    console.log('App running on port: "3000"');
+  console.log('App running on port: "3000"');
 });
